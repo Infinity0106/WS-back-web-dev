cf7c4e85789892d19b39336c7a58aefe97c0688d
Bernardo Orozco Garza <Infinity0106@users.noreply.github.com>
2020-10-15 04:19:55 +0900

    Initial

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 //create a server object:
-app.get("/", function(req, res) {
-  res.write("Hello World!..."); //write a response to the client
+app.get("/", function (req, res) {
+  res.write("Hello World! from exress"); //write a response to the client
   res.end(); //end the response
 });
 
-app.listen(8080, function() {
+app.listen(8080, function () {
   console.log("server running on 8080");
 }); //the server object listens on port 8080
